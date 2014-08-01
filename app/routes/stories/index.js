aa08a6f5a39b7e2628fa8d850cbd873df83bc63a
Will Strinz <wstrinz@gmail.com>
2014-08-01 14:20:50 -0500

    save work

--- app/routes/stories/index.js
+++ app/routes/stories/index.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(){
     return this.store.find('story');
+  },
+
+  renderTemplate: function(){
+    this.render({outlet: 'stories_index'});
   }
 });
