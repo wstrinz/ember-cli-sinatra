41ad4cee82d7f0317ae4f77301f2cab51f304ffe
Will Strinz <wstrinz@gmail.com>
2014-08-04 20:12:46 -0500

    go the mixins route?

---
+++ lib/mixins/feed.rb
@@ -0,0 +1,23 @@
+module Mixins
+  module Feed
+    def update
+      f = Feedjira::Feed.fetch_and_parse self.feed_url
+
+      if self.name != f.title
+        self.name = f.title
+        self.save
+      end
+
+      update_count = 0
+      f.entries.each do |e|
+        if Story.where(url: e.url).empty?
+          update_count += 1
+          s = Story.from_entry(e)
+          s.feed = self
+          s.save
+        end
+      end
+      logger.info "Feed #{name} updated #{update_count} stories"
+    end
+  end
+end
--- lib/models/feed.rb
+++ lib/models/feed.rb
@@ -1,23 +1,5 @@
+require_relative '../mixins/feed'
 class Feed < ActiveRecord::Base
   has_many :stories
-
-  def update
-    f = Feedjira::Feed.fetch_and_parse self.feed_url
-
-    if self.name != f.title
-      self.name = f.title
-      self.save
-    end
-
-    update_count = 0
-    f.entries.each do |e|
-      if Story.where(url: e.url).empty?
-        update_count += 1
-        s = Story.from_entry(e)
-        s.feed = self
-        s.save
-      end
-    end
-    logger.info "Feed #{name} updated #{update_count} stories"
-  end
+  include Mixins::Feed
 end
